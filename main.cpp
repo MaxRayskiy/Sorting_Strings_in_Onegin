b5823e40f9057374361ffd5628488ba7a375314e
Max <max.rayskiy@gmail.com>
2019-09-11 01:18:56 +0300

    two_sorts

--- main.cpp
+++ main.cpp
@@ -9,15 +9,18 @@ class SortStringsInFile {
     FILE *input_text;
     long file_size;
     std::vector<char*> str_begins;
+    std::vector<char *> str_ends;
     char *full_text;
 
     inline static bool IsAlphaOrNum(const char symbol); // true, если буква или цифра
-    static int Cmp(const void *f, const void *s);
+    static int CmpInOrder(const void *f, const void *s);
+    static int CmpFromEnd(const void *f, const void *s);
 
 public:
     SortStringsInFile();
     void ReadText();
     void SortInOrder();
+    void SortFromEnd();
 
     ~SortStringsInFile();
 };
@@ -26,6 +29,7 @@ int main() {
     SortStringsInFile Onegin;
     Onegin.ReadText();
     Onegin.SortInOrder();
+    Onegin.SortFromEnd();
 
     return 0;
 }
@@ -50,38 +54,25 @@ void SortStringsInFile::ReadText() {
     while ( c != EOF) {
         full_text[i++] = static_cast<char>(c);
 
-        if (c == 10) // 10 == (int)'\n'
+        if (c == 10) {// 10 == (int)'\n'
             str_begins.push_back(&full_text[i]);
+
+            if (i > 1) {
+                str_ends.push_back(&full_text[i - 2]);
+            }
+        }
 
         c = fgetc(input_text);
     }
-}
-
-SortStringsInFile::~SortStringsInFile() {
-    fclose(input_text);
-    delete [] full_text;
-}
-
-void SortStringsInFile::SortInOrder() {
-    std::qsort(&str_begins[0], str_begins.size(), sizeof(char *), Cmp);
-
-    FILE *output = fopen("..//Sorted_In_Order.txt", "w+");
-    for (const char *ch: str_begins) {
-        for (int i = 0; ;++i) {
-            std::fputc(static_cast<int>(ch[i]), output);
-
-            if(ch[i] == '\n' || ch[i] == EOF)
-                break;
-        }
-    }
-    fclose(output);
+    str_ends.push_back(&full_text[file_size - 2]);
 }
 
 inline bool SortStringsInFile::IsAlphaOrNum(const char symbol) {  // true, если буква или цифра
     return (('a' <= symbol && symbol <= 'z') || ( 'A' <= symbol && symbol <= 'Z') || ('0' <= symbol && symbol <= '9'));
 }
 
-int SortStringsInFile::Cmp(const void *f, const void *s) {
+
+int SortStringsInFile::CmpInOrder(const void *f, const void *s) {
     const char *first = *(const char**) f;
     const char *second = *(const char**) s;
 
@@ -108,4 +99,73 @@ int SortStringsInFile::Cmp(const void *f, const void *s) {
         else
             continue;
     }
+}
+
+void SortStringsInFile::SortInOrder() {
+    std::qsort(&str_begins[0], str_begins.size(), sizeof(char *), CmpInOrder);
+
+    FILE *output = fopen("..//Sorted_In_Order.txt", "w+");
+    for (const char *ch: str_begins) {
+        for (int i = 0; ;++i) {
+            std::fputc(static_cast<int>(ch[i]), output);
+
+            if(ch[i] == '\n' || ch[i] == EOF)
+                break;
+        }
+    }
+    fclose(output);
+}
+
+int SortStringsInFile::CmpFromEnd(const void *f, const void *s){
+    const char *first = *(const char**) f;
+    const char *second = *(const char**) s;
+
+    int i = 1, j = 1;
+
+    while (true) {
+        --i;
+        while (!IsAlphaOrNum(first[i])) {
+            if (first[i] == '\n')
+                return -1;
+            --i;
+        }
+
+        --j;
+        while (!IsAlphaOrNum(second[j])) {
+            if (second[j] == '\n')
+                return 1;
+            --j;
+        }
+        if (first[i] < second[j])
+            return -1;
+        else if (first[i] > second[j])
+            return 1;
+        else
+            continue;
+    }
+}
+
+void SortStringsInFile::SortFromEnd() {
+    std::qsort(&str_ends[0], str_begins.size(), sizeof(char *), CmpFromEnd);
+
+    FILE *output = fopen("..//Sorted_From_the_end.txt", "w+");
+    for (const char *ch: str_ends) {
+        for(int i = 0; ; --i) {
+            if(ch[i] == '\n' || ch[i] == EOF) { //отматали на начало строки
+                while (true) {
+                    ++i;
+                    std::fputc(static_cast<int>(ch[i]), output); // и записываем в файл строку
+                    if (ch[i] == '\n' || ch[i] == EOF) // пока она не закончится
+                        break;
+                }
+                break;
+            }
+        }
+    } //работает. страшно.
+    fclose(output);
+}
+
+SortStringsInFile::~SortStringsInFile() {
+    fclose(input_text);
+    delete [] full_text;
 }
