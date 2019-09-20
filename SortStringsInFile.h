4d448cd044cbea7276eeaa707cb7357f5ba5c593
Max <max.rayskiy@gmail.com>
2019-09-20 14:10:37 +0300

    initial_commit

--- SortStringsInFile.h
+++ SortStringsInFile.h
@@ -1,14 +1,59 @@
-//
-// Created by Max on 20.09.2019.
-//
+/*! @file
+ * Sorting Onegin
+*/
+#pragma once
 
-#ifndef ONEGIN_SORTSTRINGSINFILE_H
-#define ONEGIN_SORTSTRINGSINFILE_H
+#include <algorithm>
+#include <cstdio>
+#include <iostream>
+#include <vector>
 
-
+//! @class SortStringsInFile
 class SortStringsInFile {
+    /// Указатель на входной поток
+    FILE *input_text;
 
+    /// размер файла в байтах
+    long file_size;
+
+    /// Вектор указателей на начала  строк
+    std::vector<char *> str_begins;
+
+    /// Вектор указателей на начала  строк
+    std::vector<char *> str_ends;
+
+    /// Указатель на начало текста
+    char *full_text;
+
+    ///Метод, проверяющий, что символ является латинской буквой или цифрой
+    ///@warning только латинские символы!
+    ///@todo добавить кириллицу
+    ///@param symbol Анализируемый символ
+    ///@return true, если символ является латинской буквой или цифрой
+    inline static bool IsAlphaOrNum(const char &symbol);
+
+    /// Компаратор для сортировки с начала
+    static int CmpFromBegin(const void *input_first, const void *input_second);
+
+    /// Компаратор для сортировки с конца
+    static int CmpFromEnd(const void *input_first, const void *input_second);
+
+public:
+    /// Конструктор по умолчанию
+    SortStringsInFile();
+
+    /// Считываем текст из потока
+    void ReadText();
+
+    /// Сортируем и выводим строки с начала
+    void SortFromBegin();
+
+    /// Сортируем и выводим строки с конца
+    void SortFromEnd();
+
+    /// Возвращаем исходный текст
+    void ReturnText();
+
+    /// Деструктор по умолчанию
+    ~SortStringsInFile();
 };
-
-
-#endif //ONEGIN_SORTSTRINGSINFILE_H
